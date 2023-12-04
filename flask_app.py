c60b03d037ee0cedae4fe74232698fc10da03d32
Habib Şako <habibsako@outlook.com>
2023-12-04 23:21:47 +0300

    new variables and variable classes added

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@ api = Api(app)
 
 class Users(Resource):
     def get(self):
-        data = pd.read_csv('users.csv')
+        data = pd.read_csv('kullanici.csv')
         data = data.to_dict('records')
         return {'data' : data}, 200
 
@@ -16,46 +16,69 @@ class Users(Resource):
         req_data = pd.DataFrame({
             'name'      : [json['name']],
             'age'       : [json['age']],
-            'city'      : [json['city']]
+            'city'      : [json['city']],
+            # new variables added
+            'country'      : [json['country']],
+            'email'       : [json['email']]
         })
-        data = pd.read_csv('users.csv')
+        data = pd.read_csv('kullanici.csv')
         data = pd.concat([data, req_data], ignore_index=True)
-        data.to_csv('users.csv', index=False)
+        data.to_csv('kullanici.csv', index=False)
         return {'message' : 'Record successfully added.'}, 200
 
     def delete(self):
         name = request.args['name']
-        data = pd.read_csv('users.csv')
+        data = pd.read_csv('kullanici.csv')
 
         if name in data['name'].values:
             data = data[data['name'] != name]
-            data.to_csv('users.csv', index=False)
+            data.to_csv('kullanici.csv', index=False)
             return {'message': 'Record successfully deleted.'}, 200
         else:
             return {'message': 'Record not found.'}, 404
 
 class Cities(Resource):
     def get(self):
-        data = pd.read_csv('users.csv',usecols=[2])
+        data = pd.read_csv('kullanici.csv',usecols=[2])
         data = data.to_dict('records')
         return {'data' : data}, 200
-
+    
 class Name(Resource):
     def get(self,name):
-        data = pd.read_csv('users.csv')
+        data = pd.read_csv('kullanici.csv')
         data = data.to_dict('records')
         for entry in data:
             if entry['name'] == name :
                 return {'data' : entry}, 200
-        return {'message' : 'No entry found with this name !'}, 404
-
+        return {'message' : 'No entry found with this name !'}, 401
+    
+# add email variables, check metod
+class Email(Resource):
+    def get(self,email):
+        data = pd.read_csv('kullanici.csv')
+        data = data.to_dict('records')
+        for entry in data:
+            if entry['email'] == email :
+                return {'data' : entry}, 200
+        return {'message' : 'No entry found with this name !'}, 401
+    
+# add country variables
+class Country(Resource):
+    def get(self):
+        data = pd.read_csv('kullanici.csv',usecols=[3])
+        data = data.to_dict('records')
+        return {'data' : data}, 200
 
 # Add URL endpoints
 api.add_resource(Users, '/users')
 api.add_resource(Cities, '/cities')
-api.add_resource(Name, '/<string:name>')
+api.add_resource(Name, '/isim/<string:name>')
+
+# add new endpoints
+api.add_resource(Country, '/country')
+api.add_resource(Email, '/email/string:email>')
 
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=6767)
     app.run()
