78a6306d3655b6f0387f57899b47d2287fdaa26f
Habib Şako <habibsako@outlook.com>
2023-12-27 14:07:53 +0300

    docker eklendi. Odevler klasörden gruplandı.

--- docker 27.12.2023/ornek.py
+++ docker 27.12.2023/ornek.py
@@ -9,28 +9,27 @@ class Users(Resource):
     def get(self):
         data = pd.read_csv('kullanici.csv')
         data = data.to_dict('records')
-        return {'data' : data}, 200
+        return {'data': data}, 200
 
     def post(self):
         name = request.args['name']
         age = request.args['age']
         city = request.args['city']
-        # new variables added
+        # Yeni değişkenler eklendi
         country = request.args['country']
         email = request.args['email']
         req_data = pd.DataFrame({
-            'name'      : [name]
-            'age'       : [age],
-            'city'      : [city],
-            # new variables added
-            'country'      : [country],
-            'email'       : [email]
+            'name': [name],
+            'age': [age],
+            'city': [city],
+            # Yeni değişkenler eklendi
+            'country': [country],
+            'email': [email]
         })
         data = pd.read_csv('kullanici.csv')
-        # data = pd.concat([data, req_data], ignore_index=True)
         data = data.append(req_data, ignore_index=True)
         data.to_csv('kullanici.csv', index=False)
-        return {'message' : 'Record successfully added.'}, 200
+        return {'message': 'Kayıt başarıyla eklendi.'}, 200
 
     def delete(self):
         name = request.args['name']
@@ -39,41 +38,40 @@ class Users(Resource):
         if name in data['name'].values:
             data = data[data['name'] != name]
             data.to_csv('kullanici.csv', index=False)
-            return {'message': 'Record successfully deleted.'}, 200
+            return {'message': 'Kayıt başarıyla silindi.'}, 200
         else:
-            return {'message': 'Record not found.'}, 404
+            return {'message': 'Kayıt bulunamadı.'}, 404
 
 class Cities(Resource):
     def get(self):
-        data = pd.read_csv('kullanici.csv',usecols=[2])
+        data = pd.read_csv('kullanici.csv', usecols=[2])
         data = data.to_dict('records')
-        return {'data' : data}, 200
-    
+        return {'data': data}, 200
+
 class Name(Resource):
-    def get(self,name):
+    def get(self, name):
         data = pd.read_csv('kullanici.csv')
         data = data.to_dict('records')
         for entry in data:
-            if entry['name'] == name :
-                return {'data' : entry}, 200
-        return {'message' : 'No entry found with this name !'}, 401
-    
-# add email variables, check metod
+            if entry['name'] == name:
+                return {'data': entry}, 200
+        return {'message': 'Bu isimle kayıt bulunamadı!'}, 401
+# add email variables, check method
 class Email(Resource):
-    def get(self,email):
+    def get(self, email):
         data = pd.read_csv('kullanici.csv')
         data = data.to_dict('records')
         for entry in data:
-            if entry['email'] == email :
-                return {'data' : entry}, 200
-        return {'message' : 'No entry found with this name !'}, 401
-    
+            if entry['email'] == email:
+                return {'data': entry}, 200
+        return {'message': 'Bu isimle kayıt bulunamadı!'}, 401
+
 # add country variables
 class Country(Resource):
     def get(self):
-        data = pd.read_csv('kullanici.csv',usecols=[3])
+        data = pd.read_csv('kullanici.csv', usecols=[3])
         data = data.to_dict('records')
-        return {'data' : data}, 200
+        return {'data': data}, 200
 
 # Add URL endpoints
 api.add_resource(Users, '/users')
@@ -82,9 +80,8 @@ api.add_resource(Name, '/isim/<string:name>')
 
 # add new endpoints
 api.add_resource(Country, '/country')
-api.add_resource(Email, '/email/string:email>')
-
+api.add_resource(Email, '/email/<string:email>')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=6767)
+    app.run(host="0.0.0.0", port=5000)
     app.run()
